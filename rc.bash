a9f3eb8282555fb59448aa6de29e06cdcc5df2e4
Brenton Holiday <8rents@gmail.com>
2025-04-04 17:31:37 -0700

    updated now script

--- rc.bash
+++ rc.bash
@@ -2,3 +2,7 @@
 
 # Bash RC File
 # The rc file is the main configuration file for the shell. It is loaded with each interactive bash shell.
+
+alias l="ls -1hFA --color"
+alias la="ls -1ahFA --color"
+alias ll="ls -lahFA --color"
